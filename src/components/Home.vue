1dc079d076545067c7fe2190478290239c4ca7e1
jayesh chavan <jchavan28@gmail.com>
2017-12-21 20:43:39 +0530

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="Test">
+    <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data() {
+        return {
+            title: 'KAALPANIK IT SERVICES'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
